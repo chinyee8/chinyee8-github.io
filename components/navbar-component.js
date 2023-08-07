3fca1399ab4f723c0cba8e9803f878b7babf74c2
Chin Yee Lai <laichinyee1101@gmail.com>
2023-08-06 22:04:43 -0400

    Update navbar-component.js
    need active and hover

--- components/navbar-component.js
+++ components/navbar-component.js
@@ -19,6 +19,7 @@ class Navbar extends HTMLElement {
       <ul class="mt-1 flex flex-row justify-end">
         <li><a href="index.html" class="mx-4">HOME</a></li>
         <li><a href=# class="mx-4">ABOUT ME</a></li>
+        <li><a href=# class="mx-4">PROJECTS</a></li>
         <li><a href=# class="mx-4">CONTACT</a></li>
       </ul>
     </div>  
