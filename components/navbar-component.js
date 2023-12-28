4b85dcc4bea71639be2238f580ef506ce2f7c3b1
Chin Yee Lai <laichinyee1101@gmail.com>
2023-12-28 18:48:48 -0500

    fix nav p1

--- components/navbar-component.js
+++ components/navbar-component.js
@@ -5,20 +5,62 @@ class Navbar extends HTMLElement {
 
   connectedCallback() {
     this.innerHTML = `
+    <script>
+      function openMobileMenu() {
+        document.getElementById("mobile-menu").style.width = "100%";
+        document.getElementById("mobile-menu").style.display = "inline-block";
+       
+      }
+
+      function closeMobileMenu() {
+        document.getElementById("mobile-menu").style.display = "none";
+      }
+
+    </script>
     <style>
       .stick{
         position: -webkit-sticky;
         position: sticky;
         top: 0;
       }
+      
+      .mobile-menu{
+        display: none;
+      }
+      
+      .menu-icon{
+        cursor: pointer;
+        display: none;
+      }
+
+      @media only screen and (max-width: 640px) { 
+        .menu-icon{
+          display: block;
+        }
+
+       
+
+        .menu{
+          display: none;
+        }
+      }
     </style>
 
-    <div class="stick flex mt-2 justify-between shadow-lg bg-white">
-      <a href="#home"><img src="files/logo2.png" class="mx-5 w-20 h-12"></img></a>
-      <ul class="mt-4 mr-12 flex flex-row justify-end">
-        <li><a href="#home" class="mx-4">HOME</a></li>
-        <li><a href="#aboutme" class="mx-4">ABOUT</a></li>
-      </ul>
+    <div class=" stick flex mt-2 justify-between shadow-lg bg-white">
+        <a href="#home"><img src="files/logo2.png" class="mx-5 w-20 h-12"></img></a>
+          <div class="mt-4 mr-12 flex flex-row justify-end"><i class="menu-icon fa-solid fa-bars justify-end flex flex-row"  onclick="openMobileMenu()"></i> </div>
+      
+        <div id="mobile-menu" class="mobile-menu mt-4 mr-12 flex flex-row justify-end">
+            <a href="#home" class="mx-4">HOME</a>
+            <a href="#aboutme" class="mx-4">ABOUT</a>
+            <a href="#project" class="mx-4">PROJECT</a>
+        </div>
+
+        <ul class="menu mt-4 mr-12 flex flex-row justify-end">
+          <li><a href="#home" class="mx-4">HOME</a></li>
+          <li><a href="#aboutme" class="mx-4">ABOUT</a></li>
+          <li><a href="#project" class="mx-4">PROJECT</a></li>
+        </ul>
     </div>  
     `;
   }
